f490c2f39abd95d6ccd612062768e01c67a0234c
sawangpong muadphet <sawangpongm@gmail.com>
2011-07-17 23:56:32 +0700

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecommict::Application.config.secret_token = '0b8471917971f1b21d3f01973a87d98bb5208581f620321d0bc7c51b44143b2fb2ebd5e3157b4dd6c9eaa4d11bf8bcde780a2bd5891c733b28b95a7cc70ccb9e'
